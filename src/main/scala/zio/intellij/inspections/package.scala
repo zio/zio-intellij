f45df9ed8793ab86afa1483fa17f9f2cf93c23f5
Igal Tabachnik <hmemcpy@gmail.com>
2019-11-04 01:06:20 +0200

    Suggesting as/asError
    Fixes #3

--- src/main/scala/zio/intellij/inspections/package.scala
+++ src/main/scala/zio/intellij/inspections/package.scala
@@ -3,6 +3,7 @@ package zio.intellij
 import org.jetbrains.plugins.scala.codeInspection.collections._
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScReferencePattern
 import org.jetbrains.plugins.scala.lang.psi.api.expr._
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameter
 
 package object inspections {
   val zio = Array("zio.ZIO")
@@ -12,6 +13,7 @@ package object inspections {
   private[inspections] val `.*>`         = invocation("*>").from(zio)
   private[inspections] val `.as`         = invocation("as").from(zio)
   private[inspections] val `.map`        = invocation("map").from(zio)
+  private[inspections] val `.mapError`   = invocation("mapError").from(zio)
   private[inspections] val `.catchAll`   = invocation("catchAll").from(zio)
   private[inspections] val `.foldCause`  = invocation("foldCause").from(zio)
   private[inspections] val `.foldCauseM` = invocation("foldCauseM").from(zio)
@@ -48,6 +50,17 @@ package object inspections {
         }
       case _ => false
     }
+  }
+
+  object `_ => x` {
+    def unapply(expr: ScExpression): Option[ScExpression] = expr match {
+      case ScFunctionExpr(Seq(x), res @ Some(_)) if underscore(x) => res
+      case _                                                      => None
+    }
+
+    // todo there must be a better way!
+    def underscore(x: ScParameter): Boolean =
+      x.isWildcard
   }
 
   object `_ => ZIO.unit` {
---
+++ src/main/scala/zio/intellij/inspections/simplifications/SimplifyAsInspection.scala
@@ -0,0 +1,33 @@
+package zio.intellij.inspections.simplifications
+
+import org.jetbrains.plugins.scala.codeInspection.collections._
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
+import zio.intellij.inspections._
+
+class SimplifyAsInspection extends ZInspection(AsSimplificationType, AsErrorSimplificationType)
+
+object AsSimplificationType extends SimplificationType {
+  override def hint: String = "Replace with .as"
+
+  override def getSimplification(expr: ScExpression): Option[Simplification] = {
+    def replacement(qual: ScExpression, arg: ScExpression) =
+      replace(expr).withText(invocationText(qual, s"as(${arg.getText}"))
+    expr match {
+      case qual `.map` `_ => x`(x) => Some(replacement(qual, x))
+      case _                       => None
+    }
+  }
+}
+
+object AsErrorSimplificationType extends SimplificationType {
+  override def hint: String = "Replace with .asError"
+
+  override def getSimplification(expr: ScExpression): Option[Simplification] = {
+    def replacement(qual: ScExpression, arg: ScExpression) =
+      replace(expr).withText(invocationText(qual, s"asError(${arg.getText}"))
+    expr match {
+      case qual `.mapError` `_ => x`(x) => Some(replacement(qual, x))
+      case _                            => None
+    }
+  }
+}
