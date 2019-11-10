7f32472f90a370c1ccd76c0f1a7a448fe49e0976
Igal Tabachnik <hmemcpy@gmail.com>
2019-11-10 16:26:26 +0200

    Suggest ZIO type alias
    Fixes #7

---
+++ src/main/scala/zio/intellij/intentions/ZTypeAnnotationIntention.scala
@@ -0,0 +1,44 @@
+package zio.intellij.intentions
+
+import com.intellij.openapi.editor.Editor
+import org.jetbrains.plugins.scala.codeInsight.intention.types._
+import org.jetbrains.plugins.scala.codeInspection.collections.isOfClassFrom
+import org.jetbrains.plugins.scala.lang.psi.api.base.types.ScTypeElement
+import org.jetbrains.plugins.scala.lang.psi.api.statements._
+import org.jetbrains.plugins.scala.lang.psi.types.ScType
+import zio.intellij.inspections.zio
+
+abstract class ZTypeAnnotationIntention extends AbstractTypeAnnotationIntention {
+  final override def getText: String = getFamilyName
+
+  override protected def descriptionStrategy: Strategy = ZStrategy {
+    case (_, declaredType) => isOfClassFrom(declaredType, zio)
+    case _                 => false
+  }
+
+  override def invocationStrategy(maybeEditor: Option[Editor]): Strategy = ZStrategy {
+    case (te, declaredType) if maybeEditor.isDefined => invoke(te, declaredType, maybeEditor.get)
+    case _ => false
+  }
+
+  protected def invoke(te: ScTypeElement, declaredType: ScType, editor: Editor): Boolean
+}
+
+object ZStrategy {
+  def apply(suggest: (ScTypeElement, ScType) => Boolean): Strategy = new Strategy {
+
+    def canSuggest(te: ScTypeElement): Boolean = te.`type`().toOption match {
+      case Some(declaredType) => suggest(te, declaredType)
+      case _                  => false
+    }
+
+    override def functionWithType(function: ScFunctionDefinition, typeElement: ScTypeElement): Boolean =
+      canSuggest(typeElement)
+
+    override def valueWithType(value: ScPatternDefinition, typeElement: ScTypeElement): Boolean =
+      canSuggest(typeElement)
+
+    override def variableWithType(variable: ScVariableDefinition, typeElement: ScTypeElement): Boolean =
+      canSuggest(typeElement)
+  }
+}
