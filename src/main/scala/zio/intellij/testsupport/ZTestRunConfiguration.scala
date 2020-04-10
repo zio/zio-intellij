67b550fcec8f5a73c88fb949cc09480352169f29
Igal Tabachnik <hmemcpy@gmail.com>
2020-04-10 18:36:16 +0300

    Removing the external test runner host. The runner is now executing the spec directly via command-line

--- src/main/scala/zio/intellij/testsupport/ZTestRunConfiguration.scala
+++ src/main/scala/zio/intellij/testsupport/ZTestRunConfiguration.scala
@@ -1,14 +1,10 @@
 package zio.intellij.testsupport
 
-import com.intellij.execution.Executor
-import com.intellij.execution.configurations.{JavaCommandLineState, RunProfileState}
-import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.InvalidDataException
 import com.intellij.psi.PsiClass
-import com.intellij.util.PathUtil
-import org.jetbrains.plugins.scala.project._
 import org.jetbrains.plugins.scala.testingSupport.test.AbstractTestRunConfiguration
-import scalariform.ScalaVersion
+import org.jetbrains.plugins.scala.testingSupport.test.testdata.ClassTestData
 
 final class ZTestRunConfiguration(
   project: Project,
@@ -22,32 +18,18 @@ final class ZTestRunConfiguration(
 
   override def allowsSbtUiRun: Boolean = true
 
-  override protected def sbtTestNameKey: String = " -- -t "
+  override def testNameKey: String = "-t"
+
+  override def classKey: String = "-s" // dummy parameter, ignored by the specs runner
 
   override def isInvalidSuite(clazz: PsiClass): Boolean = false
 
   override def getActionName: String = getName
 
-  override def runnerClassName: String = "zio.intellij.testsupport.runner.ZTestRunnerHost"
+  override def runnerClassName: String = testConfigurationData match {
+    case d: ClassTestData => d.testClassPath
+    case o                => throw new InvalidDataException(s"Test configuration kind '${o.getKind}' is not supported.")
+  }
 
   override def reporterClass: String = null
-
-  override def getState(executor: Executor, env: ExecutionEnvironment): RunProfileState = {
-    // .·´¯`(>▂<)´¯`·.
-    val scalaVersion = (for {
-      module   <- Option(getModule)
-      scalaSdk <- module.scalaSdk
-      compiler <- scalaSdk.compilerVersion
-      version  <- ScalaVersion.parse(compiler)
-    } yield version).fold("2.12")(ver => s"${ver.major}.${ver.minor}")
-
-    val state = super.getState(executor, env).asInstanceOf[JavaCommandLineState]
-    val runnersPath = PathUtil
-      .getJarPathForClass(getClass)
-      .replace("zio-intellij.jar", s"testrunner_$scalaVersion.jar")
-
-    state.getJavaParameters.getClassPath.add(runnersPath)
-
-    state
-  }
 }
--- testrunner_2.13/src/main/scala/zio/intellij/testsupport/runner/ZTestRunnerHost.java
+++
@@ -1,7 +0,0 @@
-package zio.intellij.testsupport.runner;
-
-public class ZTestRunnerHost {
-    public static void main(String[] args) {
-        ZTestRunner$.MODULE$.run(args);
-    }
-}
