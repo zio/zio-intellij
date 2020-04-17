82dcdbb69cc43f08a00938cf23359cf4aefd5728
Igal Tabachnik <hmemcpy@gmail.com>
2020-04-17 15:32:42 +0300

    Removing the old testrunner. So long!
    The enhanced test runner functionality was moved to an external project which can be added to SBT by the user

--- src/main/scala/zio/intellij/testsupport/ZTestRunConfigurationProducer.scala
+++ src/main/scala/zio/intellij/testsupport/ZTestRunConfigurationProducer.scala
@@ -61,6 +61,14 @@ final class ZTestRunConfigurationProducer
     }
   }
 
+  override def onFirstRun(
+    configuration: ConfigurationFromContext,
+    context: ConfigurationContext,
+    startRunnable: Runnable
+  ): Unit =
+    // TODO this would be a great place to prompt the user for adding the test runner support
+    super.onFirstRun(configuration, context, startRunnable)
+
   override def shouldReplace(self: ConfigurationFromContext, other: ConfigurationFromContext): Boolean =
     other.isProducedBy(classOf[ScalaApplicationConfigurationProducer])
 
