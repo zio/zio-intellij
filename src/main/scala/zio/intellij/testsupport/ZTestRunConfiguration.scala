89818a8fea1c394c5bfa5186229825928ad3988b
Igal Tabachnik <hmemcpy@gmail.com>
2020-04-18 23:25:55 +0300

    Selecting the test runner if included in the modules, falling back to the terminal console runner

--- src/main/scala/zio/intellij/testsupport/ZTestRunConfiguration.scala
+++ src/main/scala/zio/intellij/testsupport/ZTestRunConfiguration.scala
@@ -1,17 +1,34 @@
 package zio.intellij.testsupport
 
+import com.intellij.execution.configurations.{ JavaCommandLineState, JavaParameters, RunProfileState, RunnerSettings }
+import com.intellij.execution.impl.ConsoleViewImpl
+import com.intellij.execution.runners.{ ExecutionEnvironment, ProgramRunner }
+import com.intellij.execution.testDiscovery.JavaAutoRunManager
+import com.intellij.execution.testframework.TestFrameworkRunningModel
+import com.intellij.execution.testframework.autotest.{ AbstractAutoTestManager, ToggleAutoTestAction }
+import com.intellij.execution.testframework.sm.SMTestRunnerConnectionUtil
+import com.intellij.execution.testframework.sm.runner.SMTRunnerConsoleProperties
+import com.intellij.execution.testframework.sm.runner.ui.SMTRunnerConsoleView
+import com.intellij.execution.testframework.ui.BaseTestsOutputConsoleView
+import com.intellij.execution.ui.ConsoleView
+import com.intellij.execution.{ DefaultExecutionResult, ExecutionResult, Executor }
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.InvalidDataException
+import com.intellij.openapi.util.{ Getter, InvalidDataException }
 import com.intellij.psi.PsiClass
+import org.jetbrains.plugins.scala.project.ModuleExt
 import org.jetbrains.plugins.scala.testingSupport.test.AbstractTestRunConfiguration
 import org.jetbrains.plugins.scala.testingSupport.test.TestRunConfigurationForm.TestKind._
-import org.jetbrains.plugins.scala.testingSupport.test.testdata.ClassTestData
+import org.jetbrains.plugins.scala.testingSupport.test.actions.AbstractTestRerunFailedTestsAction
+import org.jetbrains.plugins.scala.testingSupport.test.testdata.{ ClassTestData, TestConfigurationData }
 
 final class ZTestRunConfiguration(
   project: Project,
   name: String,
   configurationFactory: ZTestRunConfigurationFactory
-) extends AbstractTestRunConfiguration(project, configurationFactory, name, null /* ugh*/ ) {
+) extends AbstractTestRunConfiguration(project, configurationFactory, name, null /* ugh*/ ) { self =>
+
+  private val ZTestRunnerName = "zio.intellij.testsupport.ZTestRunner"
 
   override def suitePaths: List[String] = ZSuitePaths
 
@@ -30,11 +47,74 @@ final class ZTestRunConfiguration(
   override def reporterClass: String = null
 
   override def runnerClassName: String =
-    testConfigurationData match {
+    if (Option(getModule).exists(hasTestRunner)) ZTestRunnerName
+    else fromTestConfiguration(testConfigurationData)
+
+  private def fromTestConfiguration(data: TestConfigurationData) =
+    data match {
       case d: ClassTestData => d.testClassPath
       case d if d.getKind != CLASS || d.getKind != TEST_NAME =>
         throw new InvalidDataException(s"Test configuration kind '${d.getKind}' is not supported.")
-      // todo choose runner based on the sbt configuration
-      // "zio.intellij.testsupport.ZTestRunner"
     }
+
+  private[testsupport] def shouldCreateTestConsole: Boolean =
+    runnerClassName == ZTestRunnerName
+
+  private def hasTestRunner(module: Module): Boolean =
+    module.libraries.map(_.getName).exists(_.contains("zio-test-intellij"))
+
+  override def getState(executor: Executor, env: ExecutionEnvironment): RunProfileState = {
+    val oldState = super.getState(executor, env).asInstanceOf[JavaCommandLineState]
+
+    new JavaCommandLineState(env) {
+      override def createJavaParameters(): JavaParameters = oldState.getJavaParameters
+
+      override def execute(executor: Executor, runner: ProgramRunner[_ <: RunnerSettings]): ExecutionResult = {
+        val processHandler = startProcess()
+
+        val consoleView: ConsoleView =
+          if (shouldCreateTestConsole) {
+            val consoleProperties = new SMTRunnerConsoleProperties(self, "ZIO Test", executor)
+            SMTestRunnerConnectionUtil.createAndAttachConsole("ZIO Test", processHandler, consoleProperties)
+          } else {
+            val console = new ConsoleViewImpl(project, true)
+            console.attachToProcess(processHandler)
+            console
+          }
+
+        val res = new DefaultExecutionResult(
+          consoleView,
+          processHandler,
+          createActions(consoleView, processHandler, executor): _*
+        )
+
+        registerRerunAction(consoleView, res)
+      }
+
+      private def registerRerunAction(
+        consoleView: ConsoleView,
+        res: DefaultExecutionResult
+      ): DefaultExecutionResult =
+        consoleView match {
+          case testConsole: BaseTestsOutputConsoleView =>
+            val rerunFailedTestsAction = new AbstractTestRerunFailedTestsAction(testConsole)
+            rerunFailedTestsAction.init(testConsole.getProperties)
+            rerunFailedTestsAction.setModelProvider(new Getter[TestFrameworkRunningModel] {
+              override def get: TestFrameworkRunningModel =
+                testConsole.asInstanceOf[SMTRunnerConsoleView].getResultsViewer
+            })
+            res.setRestartActions(
+              rerunFailedTestsAction,
+              new ToggleAutoTestAction() {
+                override def isDelayApplicable: Boolean = false
+
+                override def getAutoTestManager(project: Project): AbstractAutoTestManager =
+                  JavaAutoRunManager.getInstance(project)
+              }
+            )
+            res
+          case _ => res
+        }
+    }
+  }
 }
--- src/main/scala/zio/intellij/testsupport/ZTestRunConfigurationProducer.scala
+++ src/main/scala/zio/intellij/testsupport/ZTestRunConfigurationProducer.scala
@@ -3,15 +3,22 @@ package zio.intellij.testsupport
 import com.intellij.execution.Location
 import com.intellij.execution.actions.{ ConfigurationContext, ConfigurationFromContext }
 import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.ide.BrowserUtil
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.notification.{ Notification, NotificationAction, NotificationType, Notifications }
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.{ PsiDirectory, PsiElement, PsiPackage }
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef._
 import org.jetbrains.plugins.scala.runner.ScalaApplicationConfigurationProducer
 import org.jetbrains.plugins.scala.testingSupport.test.testdata.{ ClassTestData, SingleTestData }
 import org.jetbrains.plugins.scala.testingSupport.test.{ AbstractTestConfigurationProducer, TestConfigurationUtil }
+import zio.intellij.ZioIcon
 
 final class ZTestRunConfigurationProducer
     extends AbstractTestConfigurationProducer[ZTestRunConfiguration](new ZTestConfigurationType) {
+
+  private val DoNotAdvertiseZTestRunner = "zio.intellij.do.not.advertise.testrunner"
 
   override protected def suitePaths: List[String] = ZSuitePaths
 
@@ -55,14 +62,6 @@ final class ZTestRunConfigurationProducer
       runConfiguration.setName(testNamePrefixed)
     }
   }
-
-  override def onFirstRun(
-    configuration: ConfigurationFromContext,
-    context: ConfigurationContext,
-    startRunnable: Runnable
-  ): Unit =
-    // TODO this would be a great place to prompt the user for adding the test runner support
-    super.onFirstRun(configuration, context, startRunnable)
 
   override def shouldReplace(self: ConfigurationFromContext, other: ConfigurationFromContext): Boolean =
     other.isProducedBy(classOf[ScalaApplicationConfigurationProducer])
