c18cb8e73baf039b15209780d4627ec84b0003a9
Igal Tabachnik <hmemcpy@gmail.com>
2020-04-28 02:16:29 +0300

    Fixing incorrect type aliases suggestion

--- src/main/scala/zio/intellij/intentions/ZTypeAnnotationIntention.scala
+++ src/main/scala/zio/intellij/intentions/ZTypeAnnotationIntention.scala
@@ -3,11 +3,13 @@ package zio.intellij.intentions
 import com.intellij.openapi.editor.Editor
 import org.jetbrains.plugins.scala.codeInsight.intention.types._
 import org.jetbrains.plugins.scala.codeInspection.collections.isOfClassFrom
+import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.{ScBindingPattern, ScTypedPattern, ScWildcardPattern}
 import org.jetbrains.plugins.scala.lang.psi.api.base.types.ScTypeElement
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScUnderscoreSection
 import org.jetbrains.plugins.scala.lang.psi.api.statements._
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameter
 import org.jetbrains.plugins.scala.lang.psi.types.ScType
 import zio.intellij.inspections.zioClasses
-
 
 abstract class ZTypeAnnotationIntention extends AbstractTypeAnnotationIntention with ZIcon {
   final override def getText: String = getFamilyName
@@ -26,6 +28,7 @@ abstract class ZTypeAnnotationIntention extends AbstractTypeAnnotationIntention 
 }
 
 object ZStrategy {
+
   def apply(suggest: (ScTypeElement, ScType) => Boolean): Strategy = new Strategy {
 
     def canSuggest(te: ScTypeElement): Boolean = te.`type`().toOption match {
@@ -39,7 +42,26 @@ object ZStrategy {
     override def valueWithType(value: ScPatternDefinition, typeElement: ScTypeElement): Boolean =
       canSuggest(typeElement)
 
-    override def variableWithType(variable: ScVariableDefinition, typeElement: ScTypeElement): Boolean =
-      canSuggest(typeElement)
+    override def variableWithType(variable: ScVariableDefinition, typeElement: ScTypeElement): Boolean = false
+
+    override def functionWithoutType(function: ScFunctionDefinition): Boolean = false
+
+    override def valueWithoutType(value: ScPatternDefinition): Boolean = false
+
+    override def variableWithoutType(variable: ScVariableDefinition): Boolean = false
+
+    override def patternWithoutType(pattern: ScBindingPattern): Boolean = false
+
+    override def wildcardPatternWithoutType(pattern: ScWildcardPattern): Boolean = false
+
+    override def patternWithType(pattern: ScTypedPattern): Boolean = false
+
+    override def parameterWithoutType(param: ScParameter): Boolean = false
+
+    override def parameterWithType(param: ScParameter): Boolean = false
+
+    override def underscoreSectionWithoutType(underscore: ScUnderscoreSection) = false
+
+    override def underscoreSectionWithType(underscore: ScUnderscoreSection) = false
   }
 }
--- src/main/scala/zio/intellij/intentions/suggestions/SuggestTypeAlias.scala
+++ src/main/scala/zio/intellij/intentions/suggestions/SuggestTypeAlias.scala
@@ -2,17 +2,17 @@ package zio.intellij.intentions.suggestions
 
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.search.GlobalSearchScope
-import org.jetbrains.plugins.scala.codeInsight.intention.types.{ ChooseTypeTextExpression, startTemplate }
+import org.jetbrains.plugins.scala.codeInsight.intention.types.{ChooseTypeTextExpression, startTemplate}
 import org.jetbrains.plugins.scala.lang.psi.TypeAdjuster
 import org.jetbrains.plugins.scala.lang.psi.api.base.types.ScTypeElement
-import org.jetbrains.plugins.scala.lang.psi.api.statements.{ ScTypeAlias, ScTypeAliasDefinition }
+import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScTypeAlias, ScTypeAliasDefinition}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory.createTypeElementFromText
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiManager
 import org.jetbrains.plugins.scala.lang.psi.types.api.designator.ScDesignatorType
-import org.jetbrains.plugins.scala.lang.psi.types.api.{ ParameterizedType, ScTypeText, UndefinedType }
+import org.jetbrains.plugins.scala.lang.psi.types.api.{ParameterizedType, ScTypeText, UndefinedType}
 import org.jetbrains.plugins.scala.lang.psi.types.recursiveUpdate.ScSubstitutor
-import org.jetbrains.plugins.scala.lang.psi.types.{ AliasType, ScType, TypePresentationContext }
+import org.jetbrains.plugins.scala.lang.psi.types.{AliasType, ScType, TypePresentationContext}
 import org.jetbrains.plugins.scala.project.ProjectContext
 import zio.intellij.intentions.ZTypeAnnotationIntention
 
@@ -22,7 +22,7 @@ final class SuggestTypeAlias extends ZTypeAnnotationIntention {
 
   override def getFamilyName: String = "Choose a more specific ZIO type alias"
 
-  def computeAliases(context: ProjectContext, scope: GlobalSearchScope): List[ScTypeAlias] = {
+  def allAliases(context: ProjectContext, scope: GlobalSearchScope): List[ScTypeAlias] = {
     val manager = ScalaPsiManager.instance(context)
     manager.getCachedClass(scope, "zio") match {
       case Some(z: ScObject) => z.aliases.toList
@@ -31,23 +31,26 @@ final class SuggestTypeAlias extends ZTypeAnnotationIntention {
   }
 
   override protected def invoke(te: ScTypeElement, declaredType: ScType, editor: Editor): Boolean = {
-    val aliases = (computeAliases(te.projectContext, te.getResolveScope).flatMap {
+    val aliases = (allAliases(te.projectContext, te.getResolveScope).collect {
       case alias: ScTypeAliasDefinition =>
         val undefParams = alias.typeParameters.map(UndefinedType(_))
         val undefSubst  = ScSubstitutor.bind(alias.typeParameters, undefParams)
-        for {
-          aliasType <- alias.aliasedType.toOption
-          subst     <- undefSubst(aliasType).conformanceSubstitutor(declaredType)
-        } yield subst.apply(ParameterizedType(ScDesignatorType(alias), undefParams))
-    } ++ topLevelType(te)).distinct
-      .sortBy(_.canonicalText.length)
+        alias.aliasedType.toOption
+          .flatMap(aliasType =>
+            undefSubst(aliasType).conformanceSubstitutor(declaredType)
+              .map(subst => subst.apply(ParameterizedType(ScDesignatorType(alias), undefParams)))
+              .collect {
+                case tpe: ScType if declaredType.conforms(tpe) => tpe
+              }
+          )
+    }.flatten ++ topLevelType(te)).distinct
 
     if (aliases.size == 1) {
       val replaced = te.replace(createTypeElementFromText(aliases.head.canonicalText, te.getContext, te))
       TypeAdjuster.markToAdjust(replaced)
     } else {
       implicit val tpc: TypePresentationContext = TypePresentationContext(te)
-      val texts                                 = aliases.map(ScTypeText(_))
+      val texts                                 = aliases.sortBy(_.presentableText.length).map(ScTypeText(_))
       val expr                                  = new ChooseTypeTextExpression(texts, ScTypeText(declaredType))
       startTemplate(te, te.getParent, expr, editor)
     }
@@ -57,10 +60,10 @@ final class SuggestTypeAlias extends ZTypeAnnotationIntention {
 
   def topLevelType(te: ScTypeElement): List[ScType] =
     te.`type`().toOption match {
-      case Some(typ) =>
-        typ.aliasType match {
+      case Some(tpe) =>
+        tpe.aliasType match {
           case Some(AliasType(_, _, Right(value))) => List(value)
-          case _                                   => List(typ)
+          case _                                   => List(tpe)
         }
       case _ => Nil
     }
