b2b4ed092f00258484d02ab301a6d03c6f1a26ed
Tim Aliberdov <timur.aliberdov@yandex.ru>
2020-04-28 23:38:13 +0300

    Warn about yielding a ZIO effect (#68)

---
+++ src/main/scala/zio/intellij/inspections/mistakes/YieldingZIOEffectInspection.scala
@@ -0,0 +1,48 @@
+package zio.intellij.inspections.mistakes
+
+import com.intellij.codeInspection.{InspectionManager, LocalQuickFix, ProblemDescriptor, ProblemHighlightType}
+import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.codeInspection.AbstractRegisteredInspection
+import org.jetbrains.plugins.scala.lang.psi.api.expr._
+import org.jetbrains.plugins.scala.util.IntentionAvailabilityChecker
+import zio.intellij.inspections.zioRef
+
+class YieldingZIOEffectInspection extends AbstractRegisteredInspection {
+
+  private def createDescriptor(
+    expr: ScExpression
+  )(implicit manager: InspectionManager, isOnTheFly: Boolean): ProblemDescriptor =
+    manager.createProblemDescriptor(
+      expr,
+      YieldingZIOEffectInspection.message,
+      isOnTheFly,
+      Array.empty[LocalQuickFix],
+      ProblemHighlightType.WEAK_WARNING
+    )
+
+  override protected def problemDescriptor(
+    element: PsiElement,
+    maybeQuickFix: Option[LocalQuickFix],
+    descriptionTemplate: String,
+    highlightType: ProblemHighlightType
+  )(implicit manager: InspectionManager, isOnTheFly: Boolean): Option[ProblemDescriptor] =
+    element match {
+      case expr: ScFor if IntentionAvailabilityChecker.checkInspection(this, expr.getParent) =>
+        expr.body match {
+          case Some(body @ zioRef(_, _)) =>
+            Some(createDescriptor(body))
+          case Some(e: ScBlock) =>
+            e.exprs.lastOption match {
+              case Some(body @ zioRef(_, _)) =>
+                Some(createDescriptor(body))
+              case None => None
+            }
+          case _ => None
+        }
+      case _ => None
+    }
+}
+
+object YieldingZIOEffectInspection {
+  val message = "Returning a ZIO inside the yield part of a for comprehension"
+}
