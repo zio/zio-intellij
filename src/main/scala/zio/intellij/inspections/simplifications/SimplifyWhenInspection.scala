55b2df3a1ec0385dc74508a26d91f236af193c5f
Tim Aliberdov <timur.aliberdov@yandex.ru>
2020-05-08 12:40:27 +0300

    Fix when simplification

--- src/main/scala/zio/intellij/inspections/simplifications/SimplifyWhenInspection.scala
+++ src/main/scala/zio/intellij/inspections/simplifications/SimplifyWhenInspection.scala
@@ -1,7 +1,7 @@
 package zio.intellij.inspections.simplifications
 
 import org.jetbrains.plugins.scala.codeInspection.collections._
-import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScExpression, ScReferenceExpression}
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
 import zio.intellij.inspections._
 
 class SimplifyWhenInspection extends ZInspection(WhenSimplificationType)
@@ -10,14 +10,12 @@ object WhenSimplificationType extends SimplificationType {
   override def hint: String = "Replace with .when"
 
   override def getSimplification(expr: ScExpression): Option[Simplification] = {
-    def replacement(ifStmt: ScExpression, tb: ScReferenceExpression, arg: ScExpression, cond: ScExpression) = {
-      val refText = if (tb == arg) tb.getText else s"${tb.getText}(${arg.getText})"
-      replace(ifStmt).withText(s"$refText.when(${cond.getText})")
-    }
+    def replacement(ifStmt: ScExpression, body: ScExpression, cond: ScExpression) =
+      replace(ifStmt).withText(s"${body.getText}.when(${cond.getText})")
 
     expr match {
-      case cond @ IfStmt(condition, zioRef(tb, e), `ZIO.unit`(_)) => Some(replacement(cond, tb, e, condition))
-      case _                                                      => None
+      case ifStmt @ IfStmt(condition, body @ zioRef(_, _), `ZIO.unit`(_)) => Some(replacement(ifStmt, body, condition))
+      case _                                                              => None
     }
   }
 }
--- src/test/scala/zio/inspections/SimplifyWhenInspectionTest.scala
+++ src/test/scala/zio/inspections/SimplifyWhenInspectionTest.scala
@@ -30,4 +30,28 @@ class SimplifyWhenInspectionTest extends ZSimplifyInspectionTest[SimplifyWhenIns
                        |ZIO.succeed(42).when(a)""".stripMargin)
     testQuickFix(text, result, hint)
   }
+
+  def test_when_direct_reference_apply(): Unit = {
+    z(s"""|val a = true
+          |${START}if (a) ZIO(42) else ZIO.unit$END""".stripMargin).assertHighlighted()
+    val text   = z(s"""|val a = true
+                     |if (a) ZIO(42) else ZIO.unit""".stripMargin)
+    val result = z(s"""|val a = true
+                       |ZIO(42).when(a)""".stripMargin)
+    testQuickFix(text, result, hint)
+  }
+
+  def test_when_complex_reference(): Unit = {
+    def base(expr: String): String =
+      s"""|val a = true
+          |val list = (1 to 5).toList
+          |val toZIO: Int => UIO[Int] = UIO.succeed(_)
+          |$expr""".stripMargin
+    val reference = "ZIO.foreach_(list)(toZIO)"
+
+    z(base(s"${START}if (a) $reference else ZIO.unit$END")).assertHighlighted()
+    val text   = z(base(s"if (a) $reference else ZIO.unit"))
+    val result = z(base(s"$reference.when(a)"))
+    testQuickFix(text, result, hint)
+  }
 }
