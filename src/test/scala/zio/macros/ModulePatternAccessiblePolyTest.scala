0d7565413202230db100ecc87478845a12d3551c
Timur Aliberdov <timur.aliberdov@yandex.ru>
2020-10-09 16:24:57 +0300

    Fix @accessible accessors for ZStream/ZSink (#160)

--- src/test/scala/zio/macros/ModulePatternAccessiblePolyTest.scala
+++ src/test/scala/zio/macros/ModulePatternAccessiblePolyTest.scala
@@ -2,18 +2,19 @@ package zio.macros
 
 import intellij.testfixtures.RichStr
 import org.jetbrains.plugins.scala.base.libraryLoaders.{IvyManagedLoader, LibraryLoader}
-import org.jetbrains.plugins.scala.lang.refactoring.ScTypePresentationExt
 import org.junit.Assert._
 
 class ModulePatternAccessiblePolyTest extends MacrosTest {
 
   override def librariesLoaders: Seq[LibraryLoader] =
-    super.librariesLoaders :+ IvyManagedLoader(zioOrg %% "zio-macros" % zioVersion)
+    super.librariesLoaders :+
+      IvyManagedLoader(zioOrg %% "zio-streams" % zioVersion, zioOrg %% "zio-macros" % zioVersion)
 
   override protected val code =
     s"""import zio._
        |import zio.blocking.Blocking
        |import zio.macros.accessible
+       |import zio.stream.{ZSink, ZStream}
        |
        |@accessible
        |object E${CARET}xample {
@@ -28,81 +29,83 @@ class ModulePatternAccessiblePolyTest extends MacrosTest {
        |    def f4(t1: T)(t2: T): UIO[T]
        |    def f5(t1: T)(implicit t2: T): UIO[T]
        |    def f6(t: T*): UIO[T]
+       |
+       |    def f7(t: T): Managed[String, T]
+       |    def f8(t: T): ZSink[R, String, T, T, List[T]]
+       |    def f9(t: T): ZStream[Any, String, T]
        |  }
        |}
        |""".stripMargin
 
-  def test_generates_accessor_function_for_field_of_the_polymorphic_service(): Unit = {
+  def test_generates_accessor_function_for_field_of_the_polymorphic_service(): Unit =
     assertEquals(
-      "def v[R, T <: Example.Foo] = zio.ZIO.service[Example.Service[R, T]].flatMap(_.v)",
+      "def v[R, T <: Example.Foo]: zio.ZIO[zio.Has[Example.Service[R, T]] with R, Throwable, T] = " +
+        "zio.ZIO.accessM(_.get[Example.Service[R, T]].v)",
       method("v").getText
     )
-    assertEquals(
-      Right("ZIO[R with Has[Example.Service[R, T]], Throwable, T]"),
-      method("v").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_function_for_method_of_the_polymorphic_service_without_argument_lists(): Unit = {
+  def test_generates_accessor_function_for_method_of_the_polymorphic_service_without_argument_lists(): Unit =
     assertEquals(
-      "def f1[R, T <: Example.Foo] = zio.ZIO.service[Example.Service[R, T]].flatMap(_.f1)",
+      "def f1[R, T <: Example.Foo]: zio.ZIO[zio.Has[Example.Service[R, T]], Nothing, T] = " +
+        "zio.ZIO.accessM(_.get[Example.Service[R, T]].f1)",
       method("f1").getText
     )
-    assertEquals(
-      Right("ZIO[Has[Example.Service[R, T]], Nothing, T]"),
-      method("f1").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_function_for_method_of_the_polymorphic_service_with_empty_argument_list(): Unit = {
+  def test_generates_accessor_function_for_method_of_the_polymorphic_service_with_empty_argument_list(): Unit =
     assertEquals(
-      "def f2[R, T <: Example.Foo]() = zio.ZIO.service[Example.Service[R, T]].flatMap(_.f2())",
+      "def f2[R, T <: Example.Foo](): zio.ZIO[zio.Has[Example.Service[R, T]], Nothing, T] = " +
+        "zio.ZIO.accessM(_.get[Example.Service[R, T]].f2())",
       method("f2").getText
     )
-    assertEquals(
-      Right("() => ZIO[Has[Example.Service[R, T]], Nothing, T]"),
-      method("f2").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_function_for_method_of_the_polymorphic_service_with_single_argument(): Unit = {
+  def test_generates_accessor_function_for_method_of_the_polymorphic_service_with_single_argument(): Unit =
     assertEquals(
-      "def f3[R, T <: Example.Foo](t: T) = zio.ZIO.service[Example.Service[R, T]].flatMap(_.f3(t))",
+      "def f3[R, T <: Example.Foo](t: T): zio.ZIO[zio.Has[Example.Service[R, T]], Nothing, T] = " +
+        "zio.ZIO.accessM(_.get[Example.Service[R, T]].f3(t))",
       method("f3").getText
     )
-    assertEquals(
-      Right("T => ZIO[Has[Example.Service[R, T]], Nothing, T]"),
-      method("f3").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_function_for_method_of_the_polymorphic_service_with_multiple_argument_lists(): Unit = {
+  def test_generates_accessor_function_for_method_of_the_polymorphic_service_with_multiple_argument_lists(): Unit =
     assertEquals(
-      "def f4[R, T <: Example.Foo](t1: T)(t2: T) = zio.ZIO.service[Example.Service[R, T]].flatMap(_.f4(t1)(t2))",
+      "def f4[R, T <: Example.Foo](t1: T)(t2: T): zio.ZIO[zio.Has[Example.Service[R, T]], Nothing, T] = " +
+        "zio.ZIO.accessM(_.get[Example.Service[R, T]].f4(t1)(t2))",
       method("f4").getText
     )
-    assertEquals(
-      Right("T => T => ZIO[Has[Example.Service[R, T]], Nothing, T]"),
-      method("f4").`type`().map(_.codeText)
-    )
-  }
 
   def test_generates_accessor_function_for_method_of_the_polymorphic_service_with_multiple_argument_lists_including_implicits()
-    : Unit = {
+    : Unit =
     assertEquals(
-      "def f5[R, T <: Example.Foo](t1: T)(implicit t2: T) = zio.ZIO.service[Example.Service[R, T]].flatMap(_.f5(t1)(t2))",
+      "def f5[R, T <: Example.Foo](t1: T)(implicit t2: T): zio.ZIO[zio.Has[Example.Service[R, T]], Nothing, T] = " +
+        "zio.ZIO.accessM(_.get[Example.Service[R, T]].f5(t1)(t2))",
       method("f5").getText
     )
-    assertEquals(
-      Right("T => T => ZIO[Has[Example.Service[R, T]], Nothing, T]"),
-      method("f5").`type`().map(_.codeText)
-    )
-  }
 
   def test_generates_accessor_function_for_method_of_the_polymorphic_service_with_varargs(): Unit =
     assertEquals(
-      "def f6[R, T <: Example.Foo](t: T*) = zio.ZIO.service[Example.Service[R, T]].flatMap(_.f6(t: _*))",
+      "def f6[R, T <: Example.Foo](t: T*): zio.ZIO[zio.Has[Example.Service[R, T]], Nothing, T] = " +
+        "zio.ZIO.accessM(_.get[Example.Service[R, T]].f6(t: _*))",
       method("f6").getText
+    )
+
+  def test_generates_accessor_function_for_method_of_the_polymorphic_service_returning_managed(): Unit =
+    assertEquals(
+      "def f7[R, T <: Example.Foo](t: T): zio.ZManaged[zio.Has[Example.Service[R, T]], String, T] = " +
+        "zio.ZManaged.accessManaged(_.get[Example.Service[R, T]].f7(t))",
+      method("f7").getText
+    )
+
+  def test_generates_accessor_function_for_method_of_the_polymorphic_service_returning_sink(): Unit =
+    assertEquals(
+      "def f8[R, T <: Example.Foo](t: T): zio.stream.ZSink[zio.Has[Example.Service[R, T]] with R, String, T, T, List[T]] = " +
+        "zio.stream.ZSink.accessSink(_.get[Example.Service[R, T]].f8(t))",
+      method("f8").getText
+    )
+
+  def test_generates_accessor_function_for_method_of_the_polymorphic_service_returning_stream(): Unit =
+    assertEquals(
+      "def f9[R, T <: Example.Foo](t: T): zio.stream.ZStream[zio.Has[Example.Service[R, T]], String, T] = " +
+        "zio.stream.ZStream.accessStream(_.get[Example.Service[R, T]].f9(t))",
+      method("f9").getText
     )
 
 }
--- src/test/scala/zio/macros/ModulePatternAccessibleTest.scala
+++ src/test/scala/zio/macros/ModulePatternAccessibleTest.scala
@@ -2,7 +2,6 @@ package zio.macros
 
 import intellij.testfixtures.RichStr
 import org.jetbrains.plugins.scala.base.libraryLoaders.{IvyManagedLoader, LibraryLoader}
-import org.jetbrains.plugins.scala.lang.refactoring.ScTypePresentationExt
 import org.junit.Assert._
 
 class ModulePatternAccessibleTest extends MacrosTest {
@@ -16,7 +15,7 @@ class ModulePatternAccessibleTest extends MacrosTest {
 import zio._
 import zio.blocking.Blocking
 import zio.macros.accessible
-import zio.stream.ZStream
+import zio.stream.{ZSink, ZStream}
 
 @accessible
 object E${CARET}xample {
@@ -46,6 +45,7 @@ object E${CARET}xample {
     def m1NonZIO(s: String): Int
     def m2NonZIO[T](s2: String = "")(p: (T, Int))(i2: Int*): Double
     def stream(n: Int): ZStream[Any, String, Int]
+    def sink(n: Int): ZSink[Any, String, Int, Int, List[Int]]
 
     val defaultImpureValue: Boolean = vNonZIO
     def defaultImpureMethod(s: String): Int = m1NonZIO(s)
@@ -53,182 +53,124 @@ object E${CARET}xample {
 }
 """
 
-  def test_generates_accessor_value_for_ZIO_field(): Unit = {
+  def test_generates_accessor_value_for_ZIO_field(): Unit =
     assertEquals(
-      "val v = zio.ZIO.service[Example.Service].flatMap(_.v)",
+      "val v: zio.ZIO[zio.Has[Example.Service] with Example.Environment, Nothing, Boolean] = " +
+        "zio.ZIO.accessM(_.get[Example.Service].v)",
       field("v").getText
     )
-    assertEquals(
-      Right("ZIO[Example.Environment with Has[Example.Service], Nothing, Boolean]"),
-      field("v").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_function_for_ZIO_method_without_arguments(): Unit = {
+  def test_generates_accessor_function_for_ZIO_method_without_arguments(): Unit =
     assertEquals(
-      "def m0 = zio.ZIO.service[Example.Service].flatMap(_.m0)",
+      "def m0: zio.ZIO[zio.Has[Example.Service] with Example.Environment, Nothing, Unit] = " +
+        "zio.ZIO.accessM(_.get[Example.Service].m0)",
       method("m0").getText
     )
-    assertEquals(
-      Right("ZIO[Example.Environment with Has[Example.Service], Nothing, Unit]"),
-      method("m0").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_function_for_ZIO_method_with_argument(): Unit = {
+  def test_generates_accessor_function_for_ZIO_method_with_argument(): Unit =
     assertEquals(
-      "def m1(s: String) = zio.ZIO.service[Example.Service].flatMap(_.m1(s))",
+      "def m1(s: String): zio.ZIO[zio.Has[Example.Service] with Example.Environment, Nothing, Int] = " +
+        "zio.ZIO.accessM(_.get[Example.Service].m1(s))",
       method("m1").getText
     )
-    assertEquals(
-      Right("String => ZIO[Example.Environment with Has[Example.Service], Nothing, Int]"),
-      method("m1").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_function_for_generic_ZIO_method_with_multiple_arg_lists_default_args_and_varargs()
-    : Unit = {
+  def test_generates_accessor_function_for_generic_ZIO_method_with_multiple_arg_lists_default_args_and_varargs(): Unit =
     assertEquals(
-      """def m2[T](s2: String = "")(p: (T, Int))(i2: Int*) = zio.ZIO.service[Example.Service].flatMap(_.m2[T](s2)(p)(i2: _*))""",
+      """def m2[T](s2: String = "")(p: (T, Int))(i2: Int*): zio.ZIO[zio.Has[Example.Service], Nothing, Double] = """ +
+        "zio.ZIO.accessM(_.get[Example.Service].m2[T](s2)(p)(i2: _*))",
       method("m2").getText
     )
-    assertEquals(
-      Right("ZIO[Has[Example.Service], Nothing, Double]"),
-      method("m2").returnType.map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_function_for_generic_ZIO_method_with_type_constraints(): Unit = {
+  def test_generates_accessor_function_for_generic_ZIO_method_with_type_constraints(): Unit =
     assertEquals(
-      """def m3[T <: Example.Foo](t: Example.Wrapped[T]) = zio.ZIO.service[Example.Service].flatMap(_.m3[T](t))""",
+      "def m3[T <: Example.Foo](t: Example.Wrapped[T]): zio.ZIO[zio.Has[Example.Service], String, List[T]] = " +
+        "zio.ZIO.accessM(_.get[Example.Service].m3[T](t))",
       method("m3").getText
     )
-    assertEquals(
-      Right("Example.Wrapped[T] => ZIO[Has[Example.Service], String, List[T]]"),
-      method("m3").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_value_for_ZIO_field_with_default_implementation(): Unit = {
+  def test_generates_accessor_value_for_ZIO_field_with_default_implementation(): Unit =
     assertEquals(
-      "val defaultPureValue = zio.ZIO.service[Example.Service].flatMap(_.defaultPureValue)",
+      "val defaultPureValue: zio.ZIO[zio.Has[Example.Service] with Example.Environment, Nothing, Boolean] = " +
+        "zio.ZIO.accessM(_.get[Example.Service].defaultPureValue)",
       field("defaultPureValue").getText
     )
-    assertEquals(
-      Right("ZIO[Example.Environment with Has[Example.Service], Nothing, Boolean]"),
-      field("defaultPureValue").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_function_for_ZIO_method_with_argument_with_default_implementation(): Unit = {
+  def test_generates_accessor_function_for_ZIO_method_with_argument_with_default_implementation(): Unit =
     assertEquals(
-      "def defaultPureMethod(s: String) = zio.ZIO.service[Example.Service].flatMap(_.defaultPureMethod(s))",
+      "def defaultPureMethod(s: String): zio.ZIO[zio.Has[Example.Service] with Example.Environment, Nothing, Int] = " +
+        "zio.ZIO.accessM(_.get[Example.Service].defaultPureMethod(s))",
       method("defaultPureMethod").getText
     )
-    assertEquals(
-      Right("String => ZIO[Example.Environment with Has[Example.Service], Nothing, Int]"),
-      method("defaultPureMethod").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_value_for_non_ZIO_field(): Unit = {
+  def test_generates_accessor_value_for_non_ZIO_field(): Unit =
     assertEquals(
-      "val vNonZIO = zio.ZIO.service[Example.Service].map(_.vNonZIO)",
+      "val vNonZIO: zio.ZIO[zio.Has[Example.Service], Throwable, Boolean] = " +
+        "zio.ZIO.access(_.get[Example.Service].vNonZIO)",
       field("vNonZIO").getText
     )
-    assertEquals(
-      Right("ZIO[Has[Example.Service], Nothing, Boolean]"),
-      field("vNonZIO").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_function_for_non_ZIO_method_without_arguments(): Unit = {
+  def test_generates_accessor_function_for_non_ZIO_method_without_arguments(): Unit =
     assertEquals(
-      "def m0NonZIO = zio.ZIO.service[Example.Service].map(_.m0NonZIO)",
+      "def m0NonZIO: zio.ZIO[zio.Has[Example.Service], Throwable, Unit] = " +
+        "zio.ZIO.access(_.get[Example.Service].m0NonZIO)",
       method("m0NonZIO").getText
     )
-    assertEquals(
-      Right("ZIO[Has[Example.Service], Nothing, Unit]"),
-      method("m0NonZIO").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_function_for_non_ZIO_method_with_argument(): Unit = {
+  def test_generates_accessor_function_for_non_ZIO_method_with_argument(): Unit =
     assertEquals(
-      "def m1NonZIO(s: String) = zio.ZIO.service[Example.Service].map(_.m1NonZIO(s))",
+      "def m1NonZIO(s: String): zio.ZIO[zio.Has[Example.Service], Throwable, Int] = " +
+        "zio.ZIO.access(_.get[Example.Service].m1NonZIO(s))",
       method("m1NonZIO").getText
     )
-    assertEquals(
-      Right("String => ZIO[Has[Example.Service], Nothing, Int]"),
-      method("m1NonZIO").`type`().map(_.codeText)
-    )
-  }
 
   def test_generates_accessor_function_for_generic_non_ZIO_method_with_multiple_arg_lists_default_args_and_varargs()
-    : Unit = {
+    : Unit =
     assertEquals(
-      """def m2NonZIO[T](s2: String = "")(p: (T, Int))(i2: Int*) = zio.ZIO.service[Example.Service].map(_.m2NonZIO[T](s2)(p)(i2: _*))""",
+      """def m2NonZIO[T](s2: String = "")(p: (T, Int))(i2: Int*): zio.ZIO[zio.Has[Example.Service], Throwable, Double] = """ +
+        "zio.ZIO.access(_.get[Example.Service].m2NonZIO[T](s2)(p)(i2: _*))",
       method("m2NonZIO").getText
     )
-    assertEquals(
-      Right("ZIO[Has[Example.Service], Nothing, Double]"),
-      method("m2NonZIO").returnType.map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_function_for_ZIO_method_returning_stream(): Unit = {
+  def test_generates_accessor_function_for_ZIO_method_returning_stream(): Unit =
     assertEquals(
-      """def stream(n: Int) = zio.ZIO.service[Example.Service].map(_.stream(n))""",
+      "def stream(n: Int): zio.stream.ZStream[zio.Has[Example.Service], String, Int] = " +
+        "zio.stream.ZStream.accessStream(_.get[Example.Service].stream(n))",
       method("stream").getText
     )
-    assertEquals(
-      Right("Int => ZIO[Has[Example.Service], Nothing, ZStream[Any, String, Int]]"),
-      method("stream").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_value_for_non_ZIO_field_with_default_implementation(): Unit = {
+  def test_generates_accessor_function_for_ZIO_method_returning_sink(): Unit =
     assertEquals(
-      "val defaultImpureValue = zio.ZIO.service[Example.Service].map(_.defaultImpureValue)",
+      "def sink(n: Int): zio.stream.ZSink[zio.Has[Example.Service], String, Int, Int, List[Int]] = " +
+        "zio.stream.ZSink.accessSink(_.get[Example.Service].sink(n))",
+      method("sink").getText
+    )
+
+  def test_generates_accessor_value_for_non_ZIO_field_with_default_implementation(): Unit =
+    assertEquals(
+      "val defaultImpureValue: zio.ZIO[zio.Has[Example.Service], Throwable, Boolean] = " +
+        "zio.ZIO.access(_.get[Example.Service].defaultImpureValue)",
       field("defaultImpureValue").getText
     )
-    assertEquals(
-      Right("ZIO[Has[Example.Service], Nothing, Boolean]"),
-      field("defaultImpureValue").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_function_for_non_ZIO_method_with_argument_with_default_implementation(): Unit = {
+  def test_generates_accessor_function_for_non_ZIO_method_with_argument_with_default_implementation(): Unit =
     assertEquals(
-      "def defaultImpureMethod(s: String) = zio.ZIO.service[Example.Service].map(_.defaultImpureMethod(s))",
+      "def defaultImpureMethod(s: String): zio.ZIO[zio.Has[Example.Service], Throwable, Int] = " +
+        "zio.ZIO.access(_.get[Example.Service].defaultImpureMethod(s))",
       method("defaultImpureMethod").getText
     )
-    assertEquals(
-      Right("String => ZIO[Has[Example.Service], Nothing, Int]"),
-      method("defaultImpureMethod").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_value_for_managed_field(): Unit = {
+  def test_generates_accessor_value_for_managed_field(): Unit =
     assertEquals(
-      "val vManaged = zio.ZManaged.service[Example.Service].flatMap(_.vManaged)",
+      "val vManaged: zio.ZManaged[zio.Has[Example.Service], String, Example.Foo] = " +
+        "zio.ZManaged.accessManaged(_.get[Example.Service].vManaged)",
       field("vManaged").getText
     )
-    assertEquals(
-      Right("ZManaged[Has[Example.Service], String, Example.Foo]"),
-      field("vManaged").`type`().map(_.codeText)
-    )
-  }
 
-  def test_generates_accessor_function_for_method_returning_managed(): Unit = {
+  def test_generates_accessor_function_for_method_returning_managed(): Unit =
     assertEquals(
-      "def mManaged(s: String) = zio.ZManaged.service[Example.Service].flatMap(_.mManaged(s))",
+      "def mManaged(s: String): zio.ZManaged[zio.Has[Example.Service], Nothing, Example.Bar] = " +
+        "zio.ZManaged.accessManaged(_.get[Example.Service].mManaged(s))",
       method("mManaged").getText
     )
-    assertEquals(
-      Right("String => ZManaged[Has[Example.Service], Nothing, Example.Bar]"),
-      method("mManaged").`type`().map(_.codeText)
-    )
-  }
 
 }
