e270b44bd541db1cff18fb1da74d9a5362a02271
Nikita Myazin <Nikita_Miazin@epam.com>
2020-12-03 10:56:46 +0300

    Fix `NeedsEnv` detection (#200)

--- src/main/scala/zio/intellij/inspections/package.scala
+++ src/main/scala/zio/intellij/inspections/package.scala
@@ -42,6 +42,7 @@ package object inspections {
     val `.tapBoth`: Qualified           = invocation("tapBoth").from(zioLikePackages)
     val `.orDie`: Qualified             = invocation("orDie").from(zioLikePackages)
     val `.provide`: Qualified           = invocation("provide").from(zioLikePackages)
+    val `.provideSomeLayer`: Qualified  = invocation("provideSomeLayer").from(zioLikePackages)
     val `.option`: Qualified            = invocation("option").from(zioLikePackages)
     val `.either`: Qualified            = invocation("either").from(zioLikePackages)
 
