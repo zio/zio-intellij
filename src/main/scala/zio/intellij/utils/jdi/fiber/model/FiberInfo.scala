eef14d08797a9270cb38d6ba3d18413e6ec3bb48
Igal Tabachnik <hmemcpy@gmail.com>
2021-03-30 14:26:20 +0300

    RIP FiberDump :(

--- src/main/scala/zio/intellij/utils/jdi/fiber/model/FiberInfo.scala
+++
@@ -1,5 +0,0 @@
-package zio.intellij.utils.jdi.fiber.model
-
-final case class FiberInfo(id: FiberId, name: Option[String], status: FiberStatus, trace: Option[ZTrace]) {
-  val renderName: String = s"${name.getOrElse("unnamed")}-${id.startTimeMillis}@${id.seqNumber}"
-}
