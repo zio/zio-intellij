385f78ac3aba31e4e7ab8f55021568678933aae0
Nikita Miazin <myazin.n@list.ru>
2023-03-15 23:44:45 +0200

    415 support foldZIO* in ZIO 2

--- src/test/scala/zio/inspections/SimplifyIgnoreInspectionTest.scala
+++ src/test/scala/zio/inspections/SimplifyIgnoreInspectionTest.scala
@@ -2,7 +2,10 @@ package zio.inspections
 
 import zio.intellij.inspections.simplifications.SimplifyIgnoreInspection
 
-class SimplifyIgnoreInspectionTest extends ZSimplifyInspectionTest[SimplifyIgnoreInspection] {
+abstract class SimplifyIgnoreInspectionTest extends ZSimplifyInspectionTest[SimplifyIgnoreInspection] {
+
+  private val foldCauseM = if (isZIO1) "foldCauseM" else "foldCauseZIO"
+  private val Task       = if (isZIO1) "Task" else "ZIO.attempt"
 
   override protected val hint = "Replace with .ignore"
 
@@ -21,16 +24,21 @@ class SimplifyIgnoreInspectionTest extends ZSimplifyInspectionTest[SimplifyIgnor
   }
 
   def test_foldCauseM_ZIOUnit(): Unit = {
-    z(s"ZIO.succeed(42).${START}foldCauseM(_ => ZIO.unit, _ => ZIO.unit)$END").assertHighlighted()
-    val text   = z("ZIO.succeed(42).foldCauseM(_ => ZIO.unit, _ => ZIO.unit)")
+    z(s"ZIO.succeed(42).$START$foldCauseM(_ => ZIO.unit, _ => ZIO.unit)$END").assertHighlighted()
+    val text   = z(s"ZIO.succeed(42).$foldCauseM(_ => ZIO.unit, _ => ZIO.unit)")
     val result = z("ZIO.succeed(42).ignore")
     testQuickFix(text, result, hint)
   }
 
   def test_either_unit_ignore(): Unit = {
-    z(s"Task(42).${START}either.unit$END").assertHighlighted()
-    val text   = z("Task(42).either.unit")
-    val result = z("Task(42).ignore")
+    z(s"$Task(42).${START}either.unit$END").assertHighlighted()
+    val text   = z(s"$Task(42).either.unit")
+    val result = z(s"$Task(42).ignore")
     testQuickFix(text, result, hint)
   }
+}
+
+class SimplifyIgnoreInspectionZIO1Test extends SimplifyIgnoreInspectionTest
+class SimplifyIgnoreInspectionZIO2Test extends SimplifyIgnoreInspectionTest {
+  override def isZIO1: Boolean = false
 }
