98b5c75cc2c8f74718732824c49a8c8115561ac3
Nikita Miazin <myazin.n@list.ru>
2023-04-02 12:13:09 +0300

    suggest ZIO.when and ZIO.unless in addition to .when and .unless

--- src/test/scala/zio/inspections/SimplifyUnlessInspectionTest.scala
+++ src/test/scala/zio/inspections/SimplifyUnlessInspectionTest.scala
@@ -2,9 +2,10 @@ package zio.inspections
 
 import zio.intellij.inspections.simplifications.SimplifyUnlessInspection
 
-class SimplifyUnlessInspectionTest extends ZSimplifyInspectionTest[SimplifyUnlessInspection] {
+abstract class SimplifyUnlessInspectionTestBase(replaceWith: String, `ZIO.unless`: String => String => String)
+    extends ZSimplifyInspectionTest[SimplifyUnlessInspection] {
 
-  override protected val hint = "Replace with .unless"
+  override protected val hint = s"Replace with $replaceWith"
 
   def test_unless_reference_to_val(): Unit = {
     def base(expr: String): String =
@@ -15,14 +16,14 @@ class SimplifyUnlessInspectionTest extends ZSimplifyInspectionTest[SimplifyUnles
     locally {
       z(base(s"${START}if (a) ZIO.unit else b$END")).assertHighlighted()
       val text   = z(base(s"if (a) ZIO.unit else b"))
-      val result = z(base(s"b.unless(a)"))
+      val result = z(base(`ZIO.unless`("a")("b")))
       testQuickFix(text, result, hint)
     }
 
     locally {
       z(base(s"${START}if (!a) b else ZIO.unit$END")).assertHighlighted()
       val text   = z(base(s"if (!a) b else ZIO.unit"))
-      val result = z(base(s"b.unless(a)"))
+      val result = z(base(`ZIO.unless`("a")("b")))
       testQuickFix(text, result, hint)
     }
   }
@@ -35,14 +36,14 @@ class SimplifyUnlessInspectionTest extends ZSimplifyInspectionTest[SimplifyUnles
     locally {
       z(base(s"${START}if (a) ZIO.unit else ZIO.succeed(42)$END")).assertHighlighted()
       val text   = z(base(s"if (a) ZIO.unit else ZIO.succeed(42)"))
-      val result = z(base(s"ZIO.succeed(42).unless(a)"))
+      val result = z(base(`ZIO.unless`("a")("ZIO.succeed(42)")))
       testQuickFix(text, result, hint)
     }
 
     locally {
       z(base(s"${START}if (!a) ZIO.succeed(42) else ZIO.unit$END")).assertHighlighted()
       val text   = z(base(s"if (!a) ZIO.succeed(42) else ZIO.unit"))
-      val result = z(base(s"ZIO.succeed(42).unless(a)"))
+      val result = z(base(`ZIO.unless`("a")("ZIO.succeed(42)")))
       testQuickFix(text, result, hint)
     }
   }
@@ -55,14 +56,14 @@ class SimplifyUnlessInspectionTest extends ZSimplifyInspectionTest[SimplifyUnles
     locally {
       z(base(s"${START}if (a) ZIO.unit else ZIO(42)$END")).assertHighlighted()
       val text   = z(base(s"if (a) ZIO.unit else ZIO(42)"))
-      val result = z(base(s"ZIO(42).unless(a)"))
+      val result = z(base(`ZIO.unless`("a")("ZIO(42)")))
       testQuickFix(text, result, hint)
     }
 
     locally {
       z(base(s"${START}if (!a) ZIO(42) else ZIO.unit$END")).assertHighlighted()
       val text   = z(base(s"if (!a) ZIO(42) else ZIO.unit"))
-      val result = z(base(s"ZIO(42).unless(a)"))
+      val result = z(base(`ZIO.unless`("a")("ZIO(42)")))
       testQuickFix(text, result, hint)
     }
   }
@@ -78,14 +79,14 @@ class SimplifyUnlessInspectionTest extends ZSimplifyInspectionTest[SimplifyUnles
     locally {
       z(base(s"${START}if (a) ZIO.unit else $reference$END")).assertHighlighted()
       val text   = z(base(s"if (a) ZIO.unit else $reference"))
-      val result = z(base(s"$reference.unless(a)"))
+      val result = z(base(`ZIO.unless`("a")(reference)))
       testQuickFix(text, result, hint)
     }
 
     locally {
       z(base(s"${START}if (!a) $reference else ZIO.unit$END")).assertHighlighted()
       val text   = z(base(s"if (!a) $reference else ZIO.unit"))
-      val result = z(base(s"$reference.unless(a)"))
+      val result = z(base(`ZIO.unless`("a")(reference)))
       testQuickFix(text, result, hint)
     }
   }
@@ -100,14 +101,14 @@ class SimplifyUnlessInspectionTest extends ZSimplifyInspectionTest[SimplifyUnles
     locally {
       z(base(s"${START}if (a) ZIO.unit else $methodCall$END")).assertHighlighted()
       val text   = z(base(s"if (a) ZIO.unit else $methodCall"))
-      val result = z(base(s"$methodCall.unless(a)"))
+      val result = z(base(`ZIO.unless`("a")(methodCall)))
       testQuickFix(text, result, hint)
     }
 
     locally {
       z(base(s"${START}if (!a) $methodCall else ZIO.unit$END")).assertHighlighted()
       val text   = z(base(s"if (!a) $methodCall else ZIO.unit"))
-      val result = z(base(s"$methodCall.unless(a)"))
+      val result = z(base(`ZIO.unless`("a")(methodCall)))
       testQuickFix(text, result, hint)
     }
   }
@@ -122,22 +123,28 @@ class SimplifyUnlessInspectionTest extends ZSimplifyInspectionTest[SimplifyUnles
     locally {
       z(base(s"${START}if (!(!a)) ZIO.unit else b$END")).assertHighlighted()
       val text   = z(base(s"if (!(!a)) ZIO.unit else b"))
-      val result = z(base(s"b.unless(a)"))
+      val result = z(base(`ZIO.unless`("a")("b")))
       testQuickFix(text, result, hint)
     }
 
     locally {
       z(base(s"${START}if (!(!(!a))) b else ZIO.unit$END")).assertHighlighted()
       val text   = z(base(s"if (!(!(!a))) b else ZIO.unit"))
-      val result = z(base(s"b.unless(a)"))
+      val result = z(base(`ZIO.unless`("a")("b")))
       testQuickFix(text, result, hint)
     }
   }
+
+}
+
+class SimplifyUnlessInspectionTest
+    extends SimplifyUnlessInspectionTestBase(".unless", cond => body => s"$body.unless($cond)") {
 
   def test_unless_complex_expression_containing_multiple_tokens(): Unit = {
     def base(expr: String): String =
       s"""|val a = true
           |$expr""".stripMargin
+
     val complexExpr = """ZIO.succeed(42) *> ZIO.succeed("wrap this")"""
 
     locally {
@@ -180,5 +187,67 @@ class SimplifyUnlessInspectionTest extends ZSimplifyInspectionTest[SimplifyUnles
       testQuickFix(text, result, hint)
     }
   }
+}
+class SimplifyZIOUnlessInspectionTest
+    extends SimplifyUnlessInspectionTestBase("ZIO.unless", cond => body => s"ZIO.unless($cond)($body)") {
 
+  def test_unless_complex_expression_containing_multiple_tokens(): Unit = {
+    def base(expr: String): String =
+      s"""|val a = true
+          |$expr""".stripMargin
+
+    val complexExpr = """ZIO.succeed(42) *> ZIO.succeed("wrap this")"""
+
+    locally {
+      z(base(s"${START}if (a) ZIO.unit else $complexExpr$END")).assertHighlighted()
+      val text   = z(base(s"if (a) ZIO.unit else $complexExpr"))
+      val result = z(base(s"ZIO.unless(a)($complexExpr)"))
+      testQuickFix(text, result, hint)
+    }
+
+    locally {
+      z(base(s"${START}if (!a) $complexExpr else ZIO.unit$END")).assertHighlighted()
+      val text   = z(base(s"if (!a) $complexExpr else ZIO.unit"))
+      val result = z(base(s"ZIO.unless(a)($complexExpr)"))
+      testQuickFix(text, result, hint)
+    }
+  }
+
+  def test_unless_complex_expression_containing_for_comprehension(): Unit = {
+    def base(expr: String): String =
+      s"""|val a = true
+          |$expr""".stripMargin
+
+    val complexExpr =
+      """for {
+        | i <- ZIO.succeed(42)
+        | s <- ZIO.succeed("wrap this")
+        |} yield s * i""".stripMargin
+
+    locally {
+      z(base(s"${START}if (a) ZIO.unit else $complexExpr$END")).assertHighlighted()
+      val text = z(base(s"if (a) ZIO.unit else $complexExpr"))
+      val result = z {
+        base {
+          s"""ZIO.unless(a) {
+             |  $complexExpr
+             |}""".stripMargin
+        }
+      }
+      testQuickFix(text, result, hint)
+    }
+
+    locally {
+      z(base(s"${START}if (!a) $complexExpr else ZIO.unit$END")).assertHighlighted()
+      val text = z(base(s"if (!a) $complexExpr else ZIO.unit"))
+      val result = z {
+        base {
+          s"""ZIO.unless(a) {
+             |  $complexExpr
+             |}""".stripMargin
+        }
+      }
+      testQuickFix(text, result, hint)
+    }
+  }
 }
