5383153b04be71c43f8825edcfe480e7facc6ba8
Igal Tabachnik <hmemcpy@gmail.com>
2023-12-23 14:59:03 +0200

    Regex hell

--- src/main/scala/zio/intellij/testsupport/ZTestFrameworkConsoleProperties.scala
+++ src/main/scala/zio/intellij/testsupport/ZTestFrameworkConsoleProperties.scala
@@ -9,6 +9,9 @@ import com.intellij.util.ReflectionUtil
 import jetbrains.buildServer.messages.serviceMessages._
 import org.jetbrains.plugins.scala.testingSupport.test.{AbstractTestRunConfiguration, ScalaTestFrameworkConsoleProperties}
 
+import java.io.PrintStream
+import scala.util.control.NoStackTrace
+
 private[zio] class ZTestFrameworkConsoleProperties(configuration: AbstractTestRunConfiguration, executor: Executor)
     extends ScalaTestFrameworkConsoleProperties(configuration, "ZIO Test", executor)
     with SMCustomMessagesParsing {
@@ -26,6 +29,7 @@ private[zio] class ZTestFrameworkConsoleProperties(configuration: AbstractTestRu
     // All this is needed to emit a custom TestFailedEvent that contains the expected and actual values extracted from
     // the output of a ZIO Test. This allows displaying a clickable hyperlink to see the differences in IDEA's built-in
     // diff viewer, because it assumes a JUnit-style failure reporting ("Expected:", "Actual:") which ZIO Test doesn't do.
+    // Unfortunately, the visitor is both private and not overridable, so we have to resort to reflection to get to it.
     private lazy val underlyingTestVisitor = ReflectionUtil
       .findFieldInHierarchy(classOf[OutputToGeneralTestEventsConverter], _.getName == "myServiceMessageVisitor")
       .get(self)
@@ -38,20 +42,36 @@ private[zio] class ZTestFrameworkConsoleProperties(configuration: AbstractTestRu
 
   }
 
-  // Copied from [[com.intellij.execution.testframework.sm.runner.OutputToGeneralTestEventsConverter#MyServiceMessageVisitor]]
   private class ZTestVisitor(underlying: ServiceMessageVisitor) extends DefaultServiceMessageVisitor {
+    private val regexFromHell =
+      raw"\[1m.\[34m([\s\S]*).\[0m.\[0m.*\[31mwas not equal to.*\[1m.\[34m([\s\S]*?).\[0m.\[0m".r
+
     override def visitTestFailed(testFailed: TestFailed): Unit = {
       val details = testFailed.getStacktrace
+      val tf = regexFromHell
+        .findFirstMatchIn(details)
+        .map { m =>
+          val expected = unescapeAnsi(m.group(1)).trim
+          val actual   = unescapeAnsi(m.group(2)).trim
+          val ex = new Throwable with NoStackTrace {
+            override def printStackTrace(s: PrintStream): Unit =
+              s.println(details)
 
+            override def toString: String = testFailed.getFailureMessage
+          }
+          new TestFailed(testFailed.getTestName, ex, actual, expected)
+        }
+        .getOrElse {
+          testFailed
+        }
+
+      underlying.visitTestFailed(tf)
+    }
+
+    private def unescapeAnsi(s: String): String = {
       val builder = new StringBuilder()
-      new AnsiEscapeDecoder().escapeText(details.trim, ProcessOutputTypes.STDOUT, (text, _) => builder.append(text))
-      val plain = builder.result()
-      println(plain)
-
-
-
-      underlying.visitTestFailed(testFailed)
-
+      new AnsiEscapeDecoder().escapeText(s, ProcessOutputTypes.STDOUT, (text, _) => builder.append(text))
+      builder.result()
     }
 
     override def visitTestSuiteStarted(testSuiteStarted: TestSuiteStarted): Unit =
