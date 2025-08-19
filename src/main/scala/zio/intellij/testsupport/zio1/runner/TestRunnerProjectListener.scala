46aac3e3dd26ee11f677679a070a46378a9c9d4c
Oscar WvH-K <oscar@westravanholthe.nl>
2025-08-19 08:53:06 +0200

    Replace `projectOpened` with `StartupActivity`
    The method `ProjectManagerListener.projectOpened` is deprecated and will
    be removed. The `StartupActivity` is its replacement.

--- src/main/scala/zio/intellij/testsupport/zio1/runner/TestRunnerProjectListener.scala
+++ src/main/scala/zio/intellij/testsupport/zio1/runner/TestRunnerProjectListener.scala
@@ -1,10 +1,10 @@
 package zio.intellij.testsupport.zio1.runner
 
 import com.intellij.openapi.project.{Project, ProjectManagerListener}
+import com.intellij.openapi.startup.StartupActivity
 
-private[testsupport] final class TestRunnerProjectListener extends ProjectManagerListener {
-
-  override def projectOpened(project: Project): Unit =
+private[testsupport] final class TestRunnerProjectListener extends ProjectManagerListener with StartupActivity {
+  override def runActivity(project: Project): Unit =
     new TestRunnerProjectNotification(project).init()
 
   override def projectClosing(project: Project): Unit =
